58a0c74a8c24ebf60b1f417843b81b29203c8636
Jonathan Alva <exzone97@yahoo.co.id>
2020-08-27 13:07:52 +0700

    Updates

---
+++ precache-manifest.02a9afa3ff5ad1e4adc44c9190f6c68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faf4f79d06ca7aa62160f38a07d879a",
+    "url": "/frontend-exercise/index.html"
+  },
+  {
+    "revision": "b013b6e105ff27a2d182",
+    "url": "/frontend-exercise/static/css/main.e7a0914a.chunk.css"
+  },
+  {
+    "revision": "092ce08abc3b9b27c33f",
+    "url": "/frontend-exercise/static/js/2.de4ab26d.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/frontend-exercise/static/js/2.de4ab26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b013b6e105ff27a2d182",
+    "url": "/frontend-exercise/static/js/main.6d9cde7c.chunk.js"
+  },
+  {
+    "revision": "8a2d22acda8816a5095f",
+    "url": "/frontend-exercise/static/js/runtime-main.ae5c7828.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/frontend-exercise/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
